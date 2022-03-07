7e3621bdae1166b7ce22ece1473d0cc32910c1b4
chenjingdev <chenjingdev@gmail.com>
2022-03-07 23:34:19 +0900

    tailwind css 연습

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,16 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className="bg-red-500">
-      <h1 className="text-black">it works</h1>
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <span>Select Item</span>
+        <div>
+          
+        </div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
     </div>
   )
 }
